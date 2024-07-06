800ee2cebe1378b7277e7cc3046abf3f536071d1
g031n142 <fs08016939931@gmail.com>
2024-07-07 01:25:39 +0900

    コンポーネントテスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Article from "./Article";
 
 export default function Home() {
   return (
@@ -37,6 +38,17 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+
+      <div>
+          <Article
+            title ={'Testtitle'}
+            content={'TestContent'}
+          />
+          <Article
+            title ={'Test2title'}
+            content={'Test2Content'}
+          />
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
