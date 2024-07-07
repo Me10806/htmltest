9c2ba7e5d6c8a5baa9ce628c051905bb05da417f
g031n142 <fs08016939931@gmail.com>
2024-07-07 21:17:30 +0900

    URL追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,18 +25,19 @@ export default function Home() {
       <div>
           <Article
             title ={'名前'}
-            content={'Fs'}
-            fontsize={20}
+            content={'Fujiwara'}
+            fontsize={30}
             textiti={'Left'}
             titleleftpt = {0}
             titletoppt = {10}
             contentleftpt = {10}
             contenttoppt = {10}
           />
+
           <Article
             title ={'出身大学'}
-            content={'岩手'}
-            fontsize={20}
+            content={'岩手県立大学'}
+            fontsize={30}
             textiti={'Left'}
             titleleftpt = {0}
             titletoppt = {100}
@@ -45,7 +46,7 @@ export default function Home() {
           />
 
       </div>
-
+      
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -57,8 +58,10 @@ export default function Home() {
           priority
         />
       </div>
-
-
+      
+      <a href="https://www.iwate-pu.ac.jp/" target="_blank" rel="noopener noreferrer">
+        <h1>大学URL:https://www.iwate-pu.ac.jp/</h1>
+      </a>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
       </div>
