ae183a860f7adec367bf375c28533e73a765848d
g031n142 <fs08016939931@gmail.com>
2024-07-07 19:47:14 +0900

    画像Test

---
+++ src/app/Article.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+type ArticleProps = {
+    title: string;
+    content: string;
+    fontsize:number;
+    textiti:any;
+    titleleftpt:number;
+    titletoppt:number;
+    contentleftpt:number;
+    contenttoppt:number;
+};
+
+const Article = (props:ArticleProps) => {
+    const containerStyle = {
+        width: '400px',
+        height: '400px'
+      };
+      
+      const center = {
+        lat: -3.745,
+        lng: -38.523
+      };
+
+    return(
+        <div style={{fontSize: props.fontsize + 'px',textAlign:props.textiti}}>
+            <h2 style={{ marginTop: props.titletoppt + 'px', marginLeft: props.titleleftpt + 'px'}}>{props.title}</h2>
+            <p style={{ marginTop: props.contenttoppt + 'px', marginLeft: props.contentleftpt + 'px'}}>{props.content}</p>
+        </div>
+
+    )
+}
+
+export default Article;
+
+
